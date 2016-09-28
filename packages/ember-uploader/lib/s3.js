3e12a052cb36a065e6af895b22dcd652cfe303b6
Giovanni Barillari <giovanni.barillari@gmail.com>
2016-09-28 12:34:59 +0200

    Handling upload with a different backend

--- packages/ember-uploader/lib/s3.js
+++ packages/ember-uploader/lib/s3.js
@@ -40,6 +40,23 @@ export default Uploader.extend({
     });
   },
 
+  pyupload: function(file, data) {
+    var self = this;
+
+    set(this, 'isUploading', true);
+
+    return this.pysign(file, data).then(function(json) {
+      self.didSign(json);
+      var url = json.object.url.split("amazonaws.com")[0] + "amazonaws.com/";
+      var formData = self.setupFormData(file, json.upload_params);
+
+      return self.ajax(url, formData);
+    }).then(function(respData) {
+      self.didUpload(respData);
+      return respData;
+    });
+  },
+
   sign: function(file, data) {
     data = data || {};
     data.name = file.name;
@@ -55,5 +72,22 @@ export default Uploader.extend({
     };
 
     return this._ajax(settings);
-  }
+  },
+
+  pysign: function(file, data) {
+    data = data || {};
+    data.name = file.name;
+    data.size = file.size;
+
+    var settings = {
+      url: get(this, 'url'),
+      headers: get(this, 'headers'),
+      type: 'POST',
+      contentType: 'application/json',
+      data: data,
+      dataType: 'json'
+    };
+
+    return this._ajax(settings);
+  };
 });
